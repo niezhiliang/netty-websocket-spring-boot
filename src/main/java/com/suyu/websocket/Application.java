4fa23f85953df60afa58e20f96ee913734839651
聂志良 <1132263176@qq.com>
2018-12-25 08:26:33 +0800

    m

--- src/main/java/com/suyu/websocket/Application.java
+++ src/main/java/com/suyu/websocket/Application.java
@@ -3,7 +3,7 @@ package com.suyu.websocket;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "com.suyu.websocket")
+@SpringBootApplication
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class,args);
